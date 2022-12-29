23b0d009a4c47559bc050690b26f0553805324cd
ArtakSaakyan <artak.saakyan.99@mail.ru>
2022-12-29 11:12:55 +0500

    Исправил решение первой задачи

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function square(numbers) {
-  return numbers.filter(num => num % 2 == 0).map(Math.sqrt)
+  return numbers.filter(num => num % 2 == 0).map(Math.sqrt).reduce((a, b) => a + b)
 }
 
-console.log(square([5, 9, 25, 49, 81, 100, 196, 256]))
+console.log(square([0, 5, 9, 25, 49, 81, 100, 196, 256]))
